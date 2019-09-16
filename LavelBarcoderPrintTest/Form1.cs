a4bf8dfb2d2244dadbf79f2ad5787a2e497dc74b
renton18 <gakituka18@gmail.com>
2019-09-16 23:34:04 +0900

    コミット

--- LavelBarcoderPrintTest/Form1.cs
+++ LavelBarcoderPrintTest/Form1.cs
@@ -55,42 +55,69 @@ bool fdwUnderline, bool fdwStrikeOut, int fdwCharSet, int fdwOutputPrecision, in
         private void pd_PrintPage(object sender, PrintPageEventArgs e)
         {
             //デバイスコンテキスト取得
+            //e.Graphics.PageUnit = GraphicsUnit.Inch;
             IntPtr hdc = e.Graphics.GetHdc();
 
-            //バーコードの印字
-            String BarFontName = "Code 128";
-            int BarFontSize = 200;
-            //バーコードの場合は9番目の引数を0にする必要あり
-            IntPtr mFont = CreateFont(BarFontSize, 0, 0, 0, 0, false, false, false, 0, 0, 0, 0, 0, BarFontName);
+            //バーコードの印字 バーコードの場合は9番目の引数を0にする必要あり
+            IntPtr mFont = CreateFont(200, 0, 0, 0, 0, false, false, false, 0, 0, 0, 0, 0, "Code 128");
             SelectObject(hdc, mFont);
             String BarcodeData = "9999999999";
-            int xPos = 100;
-            int yPos = 150; 
-            TextOut(hdc, xPos, yPos, BarcodeData, BarcodeData.Length);
+            TextOut(hdc, 5, 305, BarcodeData, BarcodeData.Length);
             DeleteObject(mFont);
 
-            //文字列の印字
-            String BarFontName2 = "Meiryo UI";
-            int BarFontSize2 = 50;
-            IntPtr mFont2 = CreateFont(BarFontSize2, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, BarFontName2);
+            String stringFont = "Meiryo UI";
+
+            //文字列1
+            IntPtr mFont1 = CreateFont(100, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont1);
+            String BarcodeData1 = "文字列１";
+            TextOut(hdc, 5, 5, BarcodeData1, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData1));
+            DeleteObject(mFont1);
+            //文字列2
+            IntPtr mFont2 = CreateFont(100, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
             SelectObject(hdc, mFont2);
-            String BarcodeData2 = "商品名：○○○";
-            int xPos2 = 200;
-            int yPos2 = 360;
-            TextOut(hdc, xPos2, yPos2, BarcodeData2, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData2));
+            String BarcodeData2 = "文字列２";
+            TextOut(hdc, 300, 5, BarcodeData2, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData2));
             DeleteObject(mFont2);
 
-            //文字列の印字
-            String BarFontName3 = "Meiryo UI";
-            int BarFontSize3 = 100;
-            //文字列の場合は9番目の引数を128にする必要あり
-            IntPtr mFont3 = CreateFont(BarFontSize3, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, BarFontName3);
+            //文字列3
+            IntPtr mFont3 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
             SelectObject(hdc, mFont3);
-            String BarcodeData3 = "No:12345678";
-            int xPos3 = 10;
-            int yPos3 = 10;
-            TextOut(hdc, xPos3, yPos3, BarcodeData3, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData3));
+            String BarcodeData3 = "文字列３";
+            TextOut(hdc, 5, 105, BarcodeData3, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData3));
             DeleteObject(mFont3);
+            //文字列4
+            IntPtr mFont4 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont4);
+            String BarcodeData4 = "文字列４";
+            TextOut(hdc, 300, 105, BarcodeData4, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData4));
+            DeleteObject(mFont4);
+
+            //文字列5
+            IntPtr mFont5 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont5);
+            String BarcodeData5 = "文字列5";
+            TextOut(hdc, 5, 165, BarcodeData5, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData5));
+            DeleteObject(mFont5);
+            //文字列6
+            IntPtr mFont6 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont6);
+            String BarcodeData6 = "文字列6";
+            TextOut(hdc, 300, 165, BarcodeData6, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData6));
+            DeleteObject(mFont6);
+
+            //文字列7
+            IntPtr mFont7 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont7);
+            String BarcodeData7 = "文字列7";
+            TextOut(hdc, 5, 225, BarcodeData7, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData7));
+            DeleteObject(mFont7);
+            //文字列8
+            IntPtr mFont8 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFont);
+            SelectObject(hdc, mFont8);
+            String BarcodeData8 = "文字列8";
+            TextOut(hdc, 300, 225, BarcodeData8, Encoding.GetEncoding("Shift_JIS").GetByteCount(BarcodeData8));
+            DeleteObject(mFont8);
 
             //デバイスコンテキストの開放　これは必ず必要
             e.Graphics.ReleaseHdc(hdc);
